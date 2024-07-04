57c655dda6c88fae2090f5c3e9059a1e32aa7ab2
Mbformation <immvp@hotmail.fr>
2024-07-04 17:06:43 +0200

    fix: :bug: Bug fix for onChange callback

--- src/DatePicker.jsx
+++ src/DatePicker.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useRef, useState } from "react";
 import Pikaday from "pikaday";
 import "pikaday/css/pikaday.css";
 
-function DatePicker(onChange) {
-  const [date, setDate] = useState("");
+function DatePicker({ onChange }) {
   const datepickerRef = useRef();
+  const [date, setDate] = useState("");
 
   useEffect(() => {
     const datepicker = new Pikaday({
       field: datepickerRef.current,
+      format: "MM/DD/YYYY",
       onSelect: function () {
         const currentDate = this.getMoment().format("MM/DD/YYYY");
         setDate(currentDate);
@@ -17,7 +18,7 @@ function DatePicker(onChange) {
     });
 
     return () => datepicker.destroy();
-  }, []);
+  }, [onChange]);
 
   return <input type="text" ref={datepickerRef} value={date} readOnly />;
 }
